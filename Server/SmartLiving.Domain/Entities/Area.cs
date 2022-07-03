a140751feb1c964bf1acdb3b8aed0ee1f0d1f63e
Minh Tran <minhtran.2404@outlook.com>
2022-07-03 13:48:54 +0700

    Reup files from server directory

---
+++ Server/SmartLiving.Domain/Entities/Area.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SmartLiving.Domain.Entities
+{
+    public class Area : BaseEntity
+    {
+        [Required][Key] public int Id { get; set; }
+
+        [Required] public string Name { get; set; }
+
+        [Required] public int HouseId { get; set; }
+
+        public virtual House House { get; set; }
+
+        public virtual IList<Device> Devices { get; set; }
+    }
+}
---
+++ Server/SmartLiving.Domain/Entities/DeviceData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SmartLiving.Domain.Entities
+{
+    public class DeviceData : BaseEntity
+    {
+        [Required][Key] public int Id { get; set; }
+
+        [Required] public int DeviceId { get; set; }
+
+        public Device Device { get; set; }
+
+        public string Description { get; set; }
+
+        [Required] public string Location { get; set; }
+    }
+}
