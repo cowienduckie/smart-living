be484206b22c4b5cd0492c81295b84b487bc7be1
Minh Tran <minhtran.2404@outlook.com>
2022-07-23 22:54:43 +0700

    Update API
    - Close unused endpoints
    - Change default values of Command, Device
    - Open GetAllDeviceTypes enpoint
    - Update POST Command

--- Server/SmartLiving.Domain/DataTransferObjects/CommandGetDto.cs
+++ Server/SmartLiving.Domain/DataTransferObjects/CommandGetDto.cs
@@ -3,11 +3,11 @@
     public class CommandGetDto
     {
         public int Id { get; set; }
-        public int ScheduleId { get; set; }
+        public int? ScheduleId { get; set; }
         public string UserId { get; set; }
         public int DeviceId { get; set; }
         public int CommandTypeId { get; set; }
         public string Params { get; set; }
-        public bool IsExecuted { get; set; }
+        public bool IsExecuted { get; set; } = false;
     }
 }
